cc7058280f6e6d946511703ade6bea8853e74636
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-01 00:35:04 -0500

    Rudas' repositories has been created but only UserRepo has been implemented

---
+++ src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/SeatHoldRepository.java
@@ -0,0 +1,7 @@
+package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
+
+import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.SeatHold;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatHoldRepository extends JpaRepository<SeatHold, Long> {
+}
