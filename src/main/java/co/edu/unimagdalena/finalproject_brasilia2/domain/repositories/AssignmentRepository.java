dc562891724e77999b266426902e70bd00d3808b
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-02 16:25:18 -0500

    project reorganized, now I only have my repos

--- src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/AssignmentRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
-
-import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment,Long> {
-
-}
