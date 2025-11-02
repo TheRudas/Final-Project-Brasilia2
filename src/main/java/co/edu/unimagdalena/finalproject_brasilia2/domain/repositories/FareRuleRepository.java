bb61786f94b40cbc0660ab649de7c97963380e9b
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-02 16:22:20 -0500

    Gamero's repositories created but not implemented

---
+++ src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/FareRuleRepository.java
@@ -0,0 +1,8 @@
+package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
+
+import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.FareRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FareRuleRepository extends JpaRepository<FareRule,Long> {
+
+}
