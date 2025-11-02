51349bc32911049fd805becf77091b6ee2977be2
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-02 16:25:18 -0500

    project reorganized, now I only have my repos

--- src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/FareRuleRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
-
-import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.FareRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FareRuleRepository extends JpaRepository<FareRule,Long> {
-
-}
