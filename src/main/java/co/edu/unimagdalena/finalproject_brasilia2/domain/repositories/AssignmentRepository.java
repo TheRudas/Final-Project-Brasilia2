5fb5a95a0ac387a9c606157731a1452dc7554e74
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-02 16:22:20 -0500

    Gamero's repositories created but not implemented

---
+++ src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/AssignmentRepository.java
@@ -0,0 +1,8 @@
+package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
+
+import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssignmentRepository extends JpaRepository<Assignment,Long> {
+
+}
