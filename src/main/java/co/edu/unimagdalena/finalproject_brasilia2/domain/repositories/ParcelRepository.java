7729601a2d7b57336155d8282d77458ea832d945
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-02 16:25:18 -0500

    project reorganized, now I only have my repos

--- src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/ParcelRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
-
-import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.Parcel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParcelRepository extends JpaRepository<Parcel,Long> {
-}
--- src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/SeatHoldRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unimagdalena.finalproject_brasilia2.domain.repositories;
-
-import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.SeatHold;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatHoldRepository extends JpaRepository<SeatHold, Long> {
-}
