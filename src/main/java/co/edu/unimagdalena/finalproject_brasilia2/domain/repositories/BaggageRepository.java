5c0aa43c3290e49c8dea6c9918809d078daa3e87
Andres Rudas <arudas@unimagdalena.edu.co>
2025-11-17 20:35:00 -0500

    count and sum baggage by trip Id

--- src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/BaggageRepository.java
+++ src/main/java/co/edu/unimagdalena/finalproject_brasilia2/domain/repositories/BaggageRepository.java
@@ -4,6 +4,8 @@ import co.edu.unimagdalena.finalproject_brasilia2.domain.entities.Baggage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,4 +18,19 @@ public interface BaggageRepository extends JpaRepository<Baggage,Long> {
     Optional<Baggage> findByTagCode(String tagCode);
     List<Baggage> findByTicket_Passenger_Id(Long passengerId);
     List<Baggage> findAllByTicketId(Long ticketId);
+
+    //el maletero de la logica 6
+    @Query("""
+    SELECT COUNT(b)
+    FROM Baggage b
+    WHERE b.ticket.trip.id = :tripId
+    """)
+    Long countByTripId(@Param("tripId") Long tripId);
+
+    @Query("""
+    SELECT COALESCE(SUM(b.weightKg), 0)
+    FROM Baggage b
+    WHERE b.ticket.trip.id = :tripId
+    """)
+    BigDecimal sumWeightByTripId(@Param("tripId") Long tripId);
 }
